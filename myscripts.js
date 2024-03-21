4b1c1018e7ad16a28b0a788f0e6f402d08bc1f22
PauloTorrente <paulotorrentebr@gmail.com>
2024-03-21 13:04:52 +0100

    Genshin

---
+++ myscripts.js
@@ -0,0 +1,8 @@
+const booleano1 = true
+const booleano2 = false
+const booleanoAnd = booleano1 && booleano2
+const booleanoOr = booleano1 || booleano2
+const booleanoNot = !booleano1
+const booleanoMix0 = booleano1 || booleano2 && booleano1 || !booleano1 && booleano2
+let valorDivision = (17/3).toFixed(2)
+let vvalorResto = (17-7)
